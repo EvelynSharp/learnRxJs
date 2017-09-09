35a55a83829d0a66564951da3e5c776d4218dce8
Evelyn Sharp <yihongshi90@gmail.com>
2017-09-09 11:19:51 -0600

    flatMap

--- main.ts
+++ main.ts
@@ -3,32 +3,71 @@ import { Observable } from "rxjs";
 //import "rxjs/add/operator/map";
 //import "rxjs/add/operator/filter";
 
-//let numbers = [1, 5, 10];
-//let source = Observable.from(numbers);
-/*let source = Observable.create(observer => {
-  let index = 0;
-  let produceValue = () => {
-    observer.next(numbers[index++]);
-    if(index < numbers.length) {
-      setTimeout(produceValue, 250);
-    } else {
-      observer.complete();
-    }
-  }
-  produceValue();
-}).map(n => n * 2)
-  .filter( n => n > 4);
-*/
-let circle = document.getElementById("circle");
-let source = Observable.fromEvent(document, "mousemove")
-  .map( (e : MouseEvent) => {return { x: e.clientX, y: e.clientY } })
-  .filter( value => value.x < 500 )
-  .delay(300);
+let output = document.getElementById("output");
+let button = document.getElementById("button");
 
-function onNext(value) {
-  circle.style.left = value.x;
-  circle.style.top = value.y;
+let click = Observable.fromEvent(button, "click");
+
+function load(url: string) {
+  return Observable.create(observer => {
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", () => {
+      let data = JSON.parse(xhr.responseText);
+      observer.next(data);
+      observer.complete();
+    })
+    xhr.open("GET", url);
+    xhr.send();
+  })
+
 }
+
+function renderMovies(movies) {
+  movies.forEach(m => {
+    let div = document.createElement("div");
+    div.innerText = m.title;
+    output.appendChild(div);
+  })
+}
+
+click.flatMap(e => load("movie.json"))
+     .subscribe(
+//  value => console.log(value),
+//  onNext,
+      renderMovies,
+      e => console.log(`error: ${e}`),
+      () => console.log("complete")
+    )
+
+
+
+
+    //let numbers = [1, 5, 10];
+    //let source = Observable.from(numbers);
+    /*let source = Observable.create(observer => {
+      let index = 0;
+      let produceValue = () => {
+        observer.next(numbers[index++]);
+        if(index < numbers.length) {
+          setTimeout(produceValue, 250);
+        } else {
+          observer.complete();
+        }
+      }
+      produceValue();
+    }).map(n => n * 2)
+      .filter( n => n > 4);
+    */
+    //let circle = document.getElementById("circle");
+    /*let source = Observable.fromEvent(document, "mousemove")
+      .map( (e : MouseEvent) => {return { x: e.clientX, y: e.clientY } })
+      .filter( value => value.x < 500 )
+      .delay(300);
+    */
+    /*function onNext(value) {
+      circle.style.left = value.x;
+      circle.style.top = value.y;
+    }*/
 
 
 class MyObserver {
@@ -43,11 +82,3 @@ class MyObserver {
   }
 }
 //source.subscribe(new MyObserver())
-
-
-source.subscribe(
-//  value => console.log(value),
-  onNext,
-  e => console.log(`error: ${e}`),
-  () => console.log("complete")
-)
